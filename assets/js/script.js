631185bd1c30243deb2163c33fb4d2d59b1fc114
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-08-01 19:37:53 +0300

    add bootstrap an css style

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let username = ["admin01","admin02","admin03"]
+
+console.log(username)
+username.push("admin04")
+username.push("admin05")
+console.log(username)
+username.pop()
+console.log(username)
+username.unshift("admin00")
+console.log(username)
