38811b0d5d34379d292beb36b548528c1724b095
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-08-01 20:40:59 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mapping
+# page link
+# https://sprightly-druid-56144e.netlify.app
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
+const addBtnEl = document.getElementById("addBtn")
+const userListEl = document.getElementById("userlist")
+const addToListEl = document.getElementById("addToList")
 let username = ["admin01","admin02","admin03"]
 
-console.log(username)
-username.push("admin04")
-username.push("admin05")
-console.log(username)
-username.pop()
-console.log(username)
-username.unshift("admin00")
-console.log(username)
+// console.log(username)
+// username.push("admin04")
+// username.push("admin05")
+// console.log(username)
+// username.pop()
+// console.log(username)
+// username.unshift("admin00")
+// console.log(username)
+addBtnEl.addEventListener("click",function(){
+    username = []
+    let newinput = addToListEl.value
+    username.push(newinput)
+
+    for(let i in username){
+        userListEl.innerHTML += `<li style="margin-left:20px">${username[i]}</li>`
+
+    }
+    addToListEl.value = ''
+})
